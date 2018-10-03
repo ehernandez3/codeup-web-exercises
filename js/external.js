a0ddf0e7d6f56c666750166492c3ae6c69f25368
Enrique Hernandez <hernandez.x.enrique@gmail.com>
2018-10-02 23:51:33 -0500

    added extra_exercises.html and extra_exercises.js

--- js/external.js
+++ js/external.js
@@ -2,9 +2,11 @@
 
 // console.log("Hello from external JavaScript");
 //
+//------------------------1------------------------------
 // alert("Welcome to my Website!");
 //
 //
+//------------------------2------------------------------
 // var userFaveColor = prompt("What is your favorite color?");
 //
 // alert("Great, " + userFaveColor + " is my favorite color too!");
@@ -12,21 +14,47 @@
 // ------------------------OR-----------------------------
 // alert("Great," + " " + userFaveColor + " " + "is my favorite color too!");
 
-alert("Welcome to Movie Max! Would you like to rent a movie?");
 
-var costPerDay = 3;
 
-var rentDaysMermaid = prompt("How many days would you like to rent The Little Mermaid for?");
-
-var rentDaysBear = prompt("How many days would you like to rent Brother Bear for?");
-
-var rentDaysHercules = prompt("How many days would you like to rent Hercules for?");
-
-var totalRentalCost = (parseFloat(rentDaysMermaid)
-    + parseFloat(rentDaysBear)
-    + parseFloat(rentDaysHercules))
-    * parseFloat(costPerDay);
-
+//------------------------3a------------------------------
+// alert("Welcome to Movie Max! Would you like to rent a movie?");
+//
+// var costPerDay = 3;
+//
+// var rentDaysMermaid = prompt("How many days would you like to rent The Little Mermaid for?");
+//
+// var rentDaysBear = prompt("How many days would you like to rent Brother Bear for?");
+//
+// var rentDaysHercules = prompt("How many days would you like to rent Hercules for?");
+//
+// var totalRentalCost = (parseFloat(rentDaysMermaid)
+//     + parseFloat(rentDaysBear)
+//     + parseFloat(rentDaysHercules))
+//     * parseFloat(costPerDay);
+//
+// alert("Thank you for choosing Movie Max! Your total today is $" + totalRentalCost.toFixed(2));
+// ------------------------OR-----------------------------
 // alert("Thank you for choosing Movie Max! Your total today is $" + ((rentDaysMermaid * costPerDay) + (rentDaysBear * costPerDay) + (rentDaysHercules * costPerDay)));
 
-alert("Thank you for choosing Movie Max! Your total today is $" + totalRentalCost.toFixed(2));
+
+
+//------------------------3b------------------------------
+// var googlePerHour = prompt("How much does Google pay per hour?");
+// var googleHoursWorked = prompt("How many hours did you work for Google?");
+//
+// var amazonPerHour = prompt("How much does Amazon pay per hour?");
+// var amazonHoursWorked = prompt("How many hours did you work for Amazon?");
+//
+// var facebookPerHour = prompt("How much does Facebook pay per hour?");
+// var facebookHoursWorked = prompt("How many hours did you work for Facebook?");
+//
+// var totalWeekPay = (parseFloat(googlePerHour) * parseFloat(googleHoursWorked))
+//     + (parseFloat(amazonPerHour) * parseFloat(amazonHoursWorked))
+//     + (parseFloat(facebookPerHour) * parseFloat(facebookHoursWorked));
+//
+// alert("You made $" + totalWeekPay.toFixed(2) + " this week!");
+
+
+
+//------------------------3c------------------------------
+var classEnroll =
---
+++ js/extra_exercises.js
@@ -0,0 +1,28 @@
+"use strict";
+
+console.log("What up brotha!");
+
+// var vacLocation = prompt("What is your name?");
+//
+// if (vacLocation) {
+//     alert(vacLocation + " has " + vacLocation.length + " characters.");
+// }else {
+//     alert("You must type something!");
+// }
+
+
+
+// var noun = prompt("Enter a noun:");
+// var verb = prompt("Enter a verb:");
+// var adj = prompt("Enter an adjective:");
+// var adverb = prompt("Enter an adverb:");
+//
+// alert("Do you " + verb + " your " + adj + " " + noun + " " + adverb + "? That's hilarious!");
+
+
+
+var firstNumber = prompt("Enter a number:");
+var secondNumber = prompt("Enter another number:");
+// parseFloat(firstNumber);
+// parseFloat(secondNumber);
+alert(parseFloat(firstNumber) + " + " + parseFloat(secondNumber) + " = " + (parseFloat(firstNumber) + parseFloat(secondNumber)));
