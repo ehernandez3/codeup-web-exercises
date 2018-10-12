f3b38b7ce64efc2c3740fbda514eb2a4a510f66f
Enrique Hernandez <hernandez.x.enrique@gmail.com>
2018-10-12 10:45:41 -0500

    added solutions from walkthrough

--- js/objects.js
+++ js/objects.js
@@ -14,12 +14,16 @@
 
     // var person = {
     //     firstName: "Enrique",
-    //     lastName: "Hernandez",
+    //     lastName: "Hernandez"
     //
-    // }
+    // };
     // console.log(person.firstName);
     // console.log(person.lastName);
 
+//---------------------------------Justin-------------------------------------------
+
+
+//-----------------------------------END--------------------------------------------
 
     /**
      * TODO:
@@ -40,6 +44,23 @@
     // };
     // console.log(person.sayHello());
 
+//---------------------------------Justin-------------------------------------------
+//
+//     person.sayHello = function () {
+//         return "Hello "+ person.firstName + " " + person.lastName;
+//     };
+//     console.log(person.sayHello());
+//
+
+
+//=============================OR
+
+    // person.sayHello = function () {
+    //     return "Hello "+ this.firstName + " " + this.lastName;
+    // };
+    // console.log(person.sayHello());
+
+//-----------------------------------END--------------------------------------------
 
 
     /** TODO:
@@ -56,20 +77,71 @@
      * and console.log the relevant messages for each person
      */
 
-    var shoppers = [
-            {name: 'Cameron', amount: 180},
-                {name: 'Ryan', amount: 250},
-                {name: 'George', amount: 320}
+    // var shoppers = [
+    //         {name: 'Cameron', amount: 180},
+    //             {name: 'Ryan', amount: 250},
+    //             {name: 'George', amount: 320}
+    //
+    //         ];
+    //
+    // shoppers.forEach (function (shopper) {
+    //     if (shopper.amount > 200) {
+    //         console.log(shopper.name + "'s total before discounts is $" + (shopper.amount).toFixed(2) +
+    //             ". The total after discounts is $" + (shopper.amount - (shopper.amount * .12)).toFixed(2));
+    //     } else
+    //         console.log(shopper.name + " needs to pay $" + (shopper.amount).toFixed(2));
+    // });
 
-            ];
+//---------------------------------Justin-------------------------------------------
 
-    shoppers.forEach (function (shopper) {
-        if (shopper.amount > 200) {
-            console.log(shopper.name + "'s total before discounts is $" + (shopper.amount).toFixed(2) +
-                ". The total after discounts is $" + (shopper.amount - (shopper.amount * .12)).toFixed(2));
-        } else
-            console.log(shopper.name + " needs to pay $" + (shopper.amount).toFixed(2));
-    });
+    /*
+        Cameron bought 180.00 and got  $0.00 off and will pay $180.00.
+        Ryan bought $250.00 and got $30.00 off and will pay $220.00.
+        George bought $320 and got $38.40 off and will pay $281.60.
+    */
+
+
+//     var shoppers = [
+//                 {name: 'Cameron', amount: 180},
+//                     {name: 'Ryan', amount: 250},
+//                     {name: 'George', amount: 320}
+//
+//                 ];
+//
+//     function calculateDiscount(amount) {
+//
+//         var finalAmount = 0;
+//         var amountOff = 0;
+//
+//         if (amount > 200) {
+//             amountOff = amount * .12;
+//             finalAmount = amount - amountOff;
+//         } else {
+//             finalAmount = amount;
+//         }
+//
+//         return {
+//             finalAmount: finalAmount,
+//             amountOff: amountOff
+//
+//         };
+//     }
+//
+//     function displayShopperInfo(shoppers) {
+//         shoppers.forEach(function (shopper) {
+//             var amount = calculateDiscount(shopper.amount);
+//             var message = "";
+//             message += shopper.name + " bought $";
+//             message += shopper.amount.toFixed(2);
+//             message += " and got $" + amount.amountOff.toFixed(2);
+//             message += " off and will pay $ ";
+//             message += amount.finalAmount.toFixed(2) + ".";
+//             console.log(message);
+//         });
+// }
+//     displayShopperInfo(shoppers);
+
+//-----------------------------------END--------------------------------------------
 
 
     //         discountedPrice = shopper.amount * .12 + shopper.amount;
@@ -96,43 +168,84 @@
      * > console.log(books[0].author.lastName) // "Adams"
      */
 
-    var books = [
-        {
-            title: 'Green Eggs and Ham',
-            author: {
-                firstName: "Theodore",
-                lastName: "Seuss"
-            }
-        },
-        {   title: 'Gone Girl',
-            author: {
-                firstName: "Gillian",
-                lastName: "Flynn"
-            }
-        },
-        {   title: 'Who Moved My Cheese?',
-            author: {
-                firstName: "Spencer",
-                lastName: "Johnson"
-            }
-        },
-        {   title: 'Chicken Soup for the Soul',
-            author: {
-                firstName: "Jack",
-                lastName: "Canfield"
-            }
-        },
-        {   title: 'A New Earth',
-            author: {
-                firstName: "Eckhart",
-                lastName: "Tolle"
-            }
-        }
-    ];
+    // var books = [
+    //     {
+    //         title: 'Green Eggs and Ham',
+    //         author: {
+    //             firstName: "Theodore",
+    //             lastName: "Seuss"
+    //         }
+    //     },
+    //     {   title: 'Gone Girl',
+    //         author: {
+    //             firstName: "Gillian",
+    //             lastName: "Flynn"
+    //         }
+    //     },
+    //     {   title: 'Who Moved My Cheese?',
+    //         author: {
+    //             firstName: "Spencer",
+    //             lastName: "Johnson"
+    //         }
+    //     },
+    //     {   title: 'Chicken Soup for the Soul',
+    //         author: {
+    //             firstName: "Jack",
+    //             lastName: "Canfield"
+    //         }
+    //     },
+    //     {   title: 'A New Earth',
+    //         author: {
+    //             firstName: "Eckhart",
+    //             lastName: "Tolle"
+    //         }
+    //     }
+    // ];
 
         // console.log(books[3].title);
         // console.log(books[3].author.firstName);
         // console.log(books[3].author.lastName);
+
+//---------------------------------Justin-------------------------------------------
+
+        var books = [
+                {
+                    title: 'Green Eggs and Ham',
+                    author: {
+                        firstName: "Theodore",
+                        lastName: "Seuss"
+                    }
+                },
+                {   title: 'Gone Girl',
+                    author: {
+                        firstName: "Gillian",
+                        lastName: "Flynn"
+                    }
+                },
+                {   title: 'Who Moved My Cheese?',
+                    author: {
+                        firstName: "Spencer",
+                        lastName: "Johnson"
+                    }
+                },
+                {   title: 'Chicken Soup for the Soul',
+                    author: {
+                        firstName: "Jack",
+                        lastName: "Canfield"
+                    }
+                },
+                {   title: 'A New Earth',
+                    author: {
+                        firstName: "Eckhart",
+                        lastName: "Tolle"
+                    }
+                }
+            ];
+
+
+    // console.log(books[1].author.firstName + books[]);
+
+//-----------------------------------END--------------------------------------------
 
     /**
      * TODO:
@@ -165,6 +278,20 @@
     //     console.log("---");
     // });
 
+//---------------------------------Justin-------------------------------------------
+
+        for (var i = 0; i < books.length; i += 1) {
+            var message = "";
+            message += "Book # " + (i + 1) + "\n";
+            message += "Title: " + books[i].title + "\n";
+            message += "Author: " + books[i].author.firstName + " ";
+            message += books[i].author.lastName + "\n";
+            message += "---"
+
+            console.log(message);
+        }
+
+//-----------------------------------END--------------------------------------------
 
 
     /**
@@ -177,5 +304,50 @@
      *   outputs the information described above. Refactor your loop to use your
      *   `showBookInfo` function.
      */
+    //
+    // function createBook(title, author) {
+    //
+    //     var books = [
+    //         {
+    //             title: 'Green Eggs and Ham',
+    //             author: {
+    //                 firstName: "Theodore",
+    //                 lastName: "Seuss"
+    //             }
+    //         },
+    //         {   title: 'Gone Girl',
+    //             author: {
+    //                 firstName: "Gillian",
+    //                 lastName: "Flynn"
+    //             }
+    //         },
+    //         {   title: 'Who Moved My Cheese?',
+    //             author: {
+    //                 firstName: "Spencer",
+    //                 lastName: "Johnson"
+    //             }
+    //         },
+    //         {   title: 'Chicken Soup for the Soul',
+    //             author: {
+    //                 firstName: "Jack",
+    //                 lastName: "Canfield"
+    //             }
+    //         },
+    //         {   title: 'A New Earth',
+    //             author: {
+    //                 firstName: "Eckhart",
+    //                 lastName: "Tolle"
+    //             }
+    //         }
+    //
+    //     ]
+    //
+    //
+    // } console.log(createBook());
+
+//---------------------------------Justin-------------------------------------------
+
+
+//-----------------------------------END--------------------------------------------
 
 })();
